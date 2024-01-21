0af0f93b42be912961968f6bebe1f15a80f68727
yednapk <126902325+yednapk@users.noreply.github.com>
2024-01-21 06:21:44 +0000

    conversion_operator

---
+++ basicsjs/03conversionoperator.js
@@ -0,0 +1,19 @@
+// let num=undefined;
+// let numberLogin= Number(num);
+// console.log(typeof numberLogin);
+// console.log(numberLogin);
+// means ki if convert 33=> 33
+/*
+false=>0,true=>1
+null=>0
+undefined=>Nan
+string=>Nan
+let nums="";
+let boolLogin=Boolean(nums);
+console.log(boolLogin);
+//  0=>false,1=>true; null=>false ,string =>true, empty string=> false;
+*/
+console.log(2+2+"3");
+console.log("3"+3+2);
+console.log(+"");
+console.log(+true);
